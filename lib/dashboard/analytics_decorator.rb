df8ff903dcc2cf0f56fe7cec08e1415a8ae50baf
heyimkko <enhasa@gmail.com>
2014-12-17 23:47:58 +0900

    Add some lib specs

--- lib/dashboard/analytics_decorator.rb
+++ lib/dashboard/analytics_decorator.rb
@@ -1,9 +1,12 @@
 # TODO: Cache this return, or its individual calculations
-# but invalidate once user uploads new file
+# but invalidate once user uploads new file, OR date_range changes
 class AnalyticsDecorator
   attr_reader :user, :date_range
 
-  def initialize(user, date_range)
+  # TODO: there's probably a better way to set beginning of time)
+  def initialize(user, date_range = (Time.current - 10.years..Time.current))
+    return false unless user.kind_of?(User) && date_range.kind_of?(Range)
+
     @user = user
     @date_range = date_range
   end
