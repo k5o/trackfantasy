51cd2198e386ac64428b7394c40fe4f478c7ff75
heyimkko <enhasa@gmail.com>
2014-12-23 16:43:32 -0800

    Add status to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,23 @@ class User < ActiveRecord::Base
   has_many :entries
 
   EMAIL_REGEXP = /\S+@\S+/
-
-  validates_presence_of :email, :password
-  validates_uniqueness_of :email
-  validates_confirmation_of :password
-  validates :email, format: { with: EMAIL_REGEXP }
-  validates :password, :length => { minimum: 5 }
+  STATUSES = { uninitiated: 0, active: 1, inactive: 2 }
 
   has_secure_password
+  validates_presence_of :email, :password
+  validates_uniqueness_of :email
+  validates :email, format: { with: EMAIL_REGEXP }
+  validates :password, length: { minimum: 5 }
+
+  def uninitiated?
+    status == STATUSES[:uninitiated]
+  end
+
+  def active?
+    status == STATUSES[:active]
+  end
+
+  def inactive?
+    status = STATUSES[:inactive]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223215134) do
+ActiveRecord::Schema.define(version: 20141224001045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,8 @@ ActiveRecord::Schema.define(version: 20141223215134) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest", null: false
+    t.string   "password_digest",             null: false
+    t.integer  "status",          default: 0
   end
 
 end
