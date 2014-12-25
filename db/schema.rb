8d866ae3b359bb102667c0d93c7ca535b6bb9a4e
heyimkko <enhasa@gmail.com>
2014-12-25 14:59:29 -0800

    Add active_until to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224033109) do
+ActiveRecord::Schema.define(version: 20141225225828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20141224033109) do
     t.string   "password_digest",                null: false
     t.integer  "status",             default: 0
     t.string   "stripe_customer_id"
+    t.date     "active_until"
   end
 
 end
