832742f80a6f498852645664e3341ca6738d9582
heyimkko <enhasa@gmail.com>
2014-12-25 21:36:50 -0800

    Add privacy policy

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,7 @@
 class LandingController < ApplicationController
   def index
   end
+
+  def privacy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new', as: 'login'
   get '/signup', to: 'users#new', as: 'signup'
+  get '/privacy', to: 'landing#privacy', as: 'privacy'
 end
