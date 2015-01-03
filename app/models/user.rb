62eb3bd24e888edeb2b9ce3cd83f728a5f9c8d30
heyimkko <enhasa@gmail.com>
2015-01-03 02:34:12 -0800

    Set up bcrypt correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
+include BCrypt
 class User < ActiveRecord::Base
-
   has_many :accounts
   has_many :entries
 
   EMAIL_REGEXP = /\S+@\S+/
 
   has_secure_password
+  # TODO: These validations don't currently work
   # validates_presence_of :email, :password
   # validates_uniqueness_of :email
   # validates :email, format: { with: EMAIL_REGEXP }
@@ -33,5 +34,14 @@ class User < ActiveRecord::Base
     end
 
     self.save!
+  end
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
   end
 end
