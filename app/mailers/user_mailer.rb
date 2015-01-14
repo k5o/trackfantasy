055651b7ac26de2cffc1a512f7d996ed0e6c66b4
heyimkko <enhasa@gmail.com>
2015-01-14 00:47:04 -0800

    Refactor mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "\"TrackFantasy Support\" <support@trackfantasy.com>"
 
   def reset_password_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'TrackFantasy Reset Password Instructions', bcc: "support@trackfantasy.com")
+    @reset_password_token = user.try(:reset_password_token) || "error-please-contact-support"
+    mail(to: user.email, subject: 'TrackFantasy Reset Password Instructions', bcc: "support@trackfantasy.com")
   end
 
   def user_feedback_email(user, message)
