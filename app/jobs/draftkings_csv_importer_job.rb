206d267d82255d70a60c65b19fcdaff33a302190
Eric Dykstra <eric@ericdykstra.me>
2015-01-17 15:43:51 -0800

    Handle fail case.

--- app/jobs/draftkings_csv_importer_job.rb
+++ app/jobs/draftkings_csv_importer_job.rb
@@ -5,10 +5,11 @@ class DraftkingsCsvImporterJob < ActiveJob::Base
       file_location = args[:file_location]
       user = User.find(args[:user])
       file = File.open(file_location)
+      last_entry_date = Entry.last.entered_on
       CSV.foreach(file).each do |row|
         next if row[0] == "Sport"
         entry_id = Base64.encode64("#{row[1]}#{row[2]}")
-        next if Entry.find_by_site_entry_id entry_id
+        next if Entry.find_by_site_entry_id(entry_id) && Date.strptime(row[2][0..9], '%Y-%m-%d') < last_entry_date
 
         site = Site.where(name: "draftkings").first_or_create
         player = Account.where(site: site, user: user).first_or_create
@@ -17,21 +18,25 @@ class DraftkingsCsvImporterJob < ActiveJob::Base
         winnings = row[5].gsub(/\D/,'').to_f / 100
         profit = winnings - entry_fee
 
-        entry = player.entries.create!(
-          site_entry_id: entry_id,
-          sport: row[0].downcase,
-          score: row[4],
-          position: row[3],
-          total_entries: row[7],
-          contest_title: row[1],
-          opponent_username: row[1].include?("vs.") ? row[1].split('vs. ').last : "Tournament",
-          entry_fee_in_cents: entry_fee,
-          winnings_in_cents: winnings,
-          profit: profit,
-          entered_on: Date.strptime(row[2][0..9], '%Y-%m-%d'),
-          user_id: user.id
-        )
-        puts entry.id
+        if entry = player.entries.create(
+            site_entry_id: entry_id,
+            sport: row[0].downcase,
+            score: row[4],
+            position: row[3],
+            total_entries: row[7],
+            contest_title: row[1],
+            opponent_username: row[1].include?("vs.") ? row[1].split('vs. ').last : "Tournament",
+            entry_fee_in_cents: entry_fee,
+            winnings_in_cents: winnings,
+            profit: profit,
+            entered_on: Date.strptime(row[2][0..9], '%Y-%m-%d'),
+            user_id: user.id
+          )
+          puts "#{entry.id} imported"
+        else
+          puts "#{entry.id} failed"
+          next
+        end
       end
     end
   end
--- app/jobs/fanduel_csv_importer_job.rb
+++ app/jobs/fanduel_csv_importer_job.rb
@@ -17,22 +17,26 @@ class FanduelCsvImporterJob < ActiveJob::Base
         winnings = row[11].to_f * 100
         profit = winnings - entry_fee
 
-        entry = player.entries.create!(
-          site_entry_id: row[0].gsub(/\D/, ''),
-          sport: row[1],
-          score: row[5],
-          position: row[7],
-          total_entries: row[8],
-          contest_title: row[3],
-          opponent_username: row[9],
-          entry_fee_in_cents: entry_fee,
-          winnings_in_cents: winnings,
-          link: row[12],
-          profit: profit,
-          entered_on: Date.strptime(row[2], '%Y/%m/%d'),
-          user_id: user.id
-        )
-        puts entry.id
+        if entry = player.entries.create!(
+            site_entry_id: row[0].gsub(/\D/, ''),
+            sport: row[1],
+            score: row[5],
+            position: row[7],
+            total_entries: row[8],
+            contest_title: row[3],
+            opponent_username: row[9],
+            entry_fee_in_cents: entry_fee,
+            winnings_in_cents: winnings,
+            link: row[12],
+            profit: profit,
+            entered_on: Date.strptime(row[2], '%Y/%m/%d'),
+            user_id: user.id
+          )
+          puts "#{entry.id} imported"
+        else
+          puts "#{entry.id} failed"
+          next
+        end
       end
     end
   end
