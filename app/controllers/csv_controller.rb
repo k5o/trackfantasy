af907fa09f3889138a07d254a017a176483cd0fe
heyimkko <enhasa@gmail.com>
2015-01-19 20:46:27 -0800

    wip

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -1,6 +1,7 @@
 class CsvController < ApplicationController
 
   def upload
+    # Check for file presence AND handle dropzone submission (normal form submission won't have any files attached)
     if params[:file]
       filename = params[:file].original_filename
       if filename.include?("fanduel")
@@ -8,6 +9,8 @@ class CsvController < ApplicationController
       elsif filename.include?("draftkings")
         DraftkingsCsvImporterJob.new.perform({file_contents: params[:file].read, user: current_user.id})
       end
+
+      render nothing: true, status: 200 and return
     end
 
     flash[:success] = "Your entries are being processed, try refreshing in a minute."
