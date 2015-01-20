19ee3dcafb93c58b1ac0c6213c6adbb100db06b9
heyimkko <enhasa@gmail.com>
2015-01-19 23:12:38 -0800

    Fix multi upload and secure against forgery

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -1,13 +1,19 @@
 class CsvController < ApplicationController
+  before_filter :authorize!
 
   def upload
     # Check for file presence AND handle dropzone submission (normal form submission won't have any files attached)
-    if params[:file]
-      filename = params[:file].original_filename
-      if filename.include?("fanduel")
-        FanduelCsvImporterJob.perform_later({file_contents: params[:file].read, user: current_user.id})
-      elsif filename.include?("draftkings")
-        DraftkingsCsvImporterJob.perform_later({file_contents: params[:file].read, user: current_user.id})
+    files = params[:file]
+
+    if files
+      files.each_pair do |file_index, file|
+        filename = file.original_filename
+
+        if filename.include?("fanduel")
+          FanduelCsvImporterJob.perform_later({file_contents: file.read, user: @user_id})
+        elsif filename.include?("draftkings")
+          DraftkingsCsvImporterJob.perform_later({file_contents: file.read, user: @user_id})
+        end
       end
 
       render nothing: true, status: 200 and return
@@ -15,5 +21,14 @@ class CsvController < ApplicationController
 
     flash[:success] = "Your entries are being processed, try refreshing in a minute."
     redirect_to dashboard_path
+  end
+
+  def authorize!
+    begin
+      verify_authenticity_token
+      @user_id = request.headers["HTTP_USER_ID"]
+    rescue
+      render nothing: true, status: 403 and return
+    end
   end
 end
