ba0a86f69af7dba69f976b0bcd215732ff22feba
heyimkko <enhasa@gmail.com>
2015-02-18 00:56:18 -0800

    Only instantiate one DJ thread

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,9 +1,9 @@
 if !Rails.const_defined?('Console') && !($0 =~ /rake$/) && !Rails.env.test?
   Rails.application.config.after_initialize do
 
-    (1..2).each do |thread_id|
+    # (1..2).each do |thread_id|
       Thread.new {
-        Thread.current[:thread_name] = "DJ Web Worker Thread #{thread_id}"
+        Thread.current[:thread_name] = "DJ Web Worker Thread #{thread_id || 1}"
         ActiveRecord::Base.connection_pool.with_connection do |conn|
           dj = Delayed::Worker.new
           Rails.logger.warn "Starting #{Thread.current[:thread_name]}"
@@ -11,6 +11,6 @@ if !Rails.const_defined?('Console') && !($0 =~ /rake$/) && !Rails.env.test?
           dj.start
         end
       }
-    end
+    # end
   end
 end
